36464a0e09042f0ddc282aece705b20aee4032a8
Grant Rosario <grantster1124@aol.com>
2017-06-06 22:57:54 -0400

    Added input and looping

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -7,13 +7,40 @@
 //
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+void PrintIntro();
+string GetGuess();
+
+
 int main()
 {
-    constexpr int WORD_LENGTH = 5;
-    cout << "Welcome to Bulls and Cows, a fun word game." << endl;
-    cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+    constexpr int GUESS_AMT = 5;
+    
+    PrintIntro();
+    for(int i = 0; i < GUESS_AMT; i++)
+    {
+        GetGuess();
+    }
     return 0;
+}
+
+void PrintIntro()
+{
+    // Introduce the game
+    constexpr int WORD_LENGTH = 9;
+    cout << "Welcome to Bulls and Cows, a fun word game.\n";
+    cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n\n";
+    return;
+}
+
+string GetGuess()
+{
+    // Get guess from player
+    cout << "Enter your guess: ";
+    string Guess = "";
+    getline(cin, Guess);
+    return Guess;
 }
